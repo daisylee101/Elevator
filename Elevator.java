f4c654a42e44d3981f27e1f1466044581da9bfa7
daisylee101 <daisyl43@nycstudents.net>
2025-05-16 16:33:07 +0000

    start project

---
+++ Elevator.java
@@ -0,0 +1,17 @@
+public class Elevator{
+
+    private int period;
+    private static int passing;
+    private boolean 1isBroken;
+    private boolean 2isBroken;
+    private boolean 3isBroken;
+    private boolean 4isBroken;
+
+    public Elevator{
+
+    }
+    public boolean isLate{}
+    public int lateTime{}
+    public boolean isBroken{}
+    public int waitTime{}
+ }
